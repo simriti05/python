b384fb2233fbc5cb12b0b86377014df8eab5efeb
simriti05 <146444829+simriti05@users.noreply.github.com>
2025-09-13 18:59:13 +0530

    Add files via upload

---
+++ loopassingment.py
@@ -0,0 +1,68 @@
+"""Question 1"""
+"""num = int(input("Enter the value of n: "))
+init=0
+while init<num:
+    init+=1
+    print(init)
+"""
+
+"""Q2: num = int(input("Enter the value of n: "))
+init=1
+while init<num:
+    num-=1
+    print(num)"""
+
+"""Q3: print("Even numbers between 1 to 100 are: ")
+init=0
+while init<100:
+    init+=2
+    print(init)
+"""
+
+
+"""Q4: n = int(input("Enter the number n: "))
+total_sum = 0
+for i in range(1, n + 1):
+    total_sum += i
+
+print("Sum of numbers from 1 to", n, "is:", total_sum)"""
+
+"""Q5: n=int(input("Enter the value of n: "))
+total_sum=0
+for i in range(2,n+1,2):
+    total_sum +=i
+
+print("Sum of numbers from 1 to ", n,"is:",total_sum)"""
+
+"""Q6: arr=[1,2,3,4,5,6,7,8,9,10]
+num = int(input("Multiplication of table of : "))
+multi=0
+for i in range(1,11):
+    multi+=1
+    print(num,"X",i,"=",multi*num)"""
+
+"""Q7: num = int(input("Enter a number: "))
+count = 0
+
+if num == 0:
+    count = 1
+
+while num > 0:
+    num = num // 10  
+    count = count + 1
+
+print("Number of digits:", count)"""
+
+
+
+
+    
+
+
+
+
+    
+
+
+
+
